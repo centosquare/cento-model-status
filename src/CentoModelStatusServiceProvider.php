cb4bac288b026ad53a0f53853116fd462dfabe7d
Muhammad Waqas <mwaqasiu@gmail.com>
2023-01-08 16:44:34 +0500

    initial package setup

---
+++ src/CentoModelStatusServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace CentoSquare\CentoLoc;
+
+use Illuminate\Support\ServiceProvider;
+
+class CentoModelStatusServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+}
